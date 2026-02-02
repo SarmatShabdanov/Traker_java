6f18a40fb5d8e3847d8ccf82561f8ad8f008ee4a
user <expamle@gmail.com>
2026-02-02 16:32:55 +0600

    hello app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.traker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.traker to javafx.fxml;
+    exports com.example.traker;
+}
